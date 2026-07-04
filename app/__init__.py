15bc6ff5f6171b9527e9b2b3f427e0b9ccc20f84
leonel santana dos santos <leonelsantosbh@gmail.com>
2026-07-03 21:20:52 -0300

    fix: configuracao estatica para servir imagem

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ mail = Mail()
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static', static_url_path='/static')
 
     # Configuracao da SECRET_KEY
     app.config['SECRET_KEY'] = os.environ.get(
